ae953e2bcf318036967000d48efca7bb16c703a7
Tobias Wehrle <38261180+EvilWeasel@users.noreply.github.com>
2023-11-02 20:20:10 +0100

    added sick-ass font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["GeistVF"],
+        serif: ["GeistVF"],
+        mono: ["GeistMonoVF"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
