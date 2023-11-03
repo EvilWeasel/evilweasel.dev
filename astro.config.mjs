8416d46db39862e96d45df118f80a66c8ccf31f4
Tobias Wehrle <38261180+EvilWeasel@users.noreply.github.com>
2023-11-03 10:34:55 +0100

    added preact support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), preact()],
 });
